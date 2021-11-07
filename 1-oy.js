91d9de6bca32706768acd81e61c8d821c593dd42
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2021-11-07 21:38:53 +0500

    yana qowildi misolla

--- 1-oy.js
+++ 1-oy.js
@@ -42,17 +42,18 @@ console.log(max(1,2)); */
 FizzBuzz()*/
 
 // 4 - misol
-/*function showNumbers(limit){
-    for(let bir=1; bir<limit; bir++){
-        if(bir%2==0){
-            console.log(bir + ' juft son');
+/*function showNumbers(array){
+    for(let index of array){
+        if(index%2==0){
+            console.log(index + ' juft son');
         }
-        if(bir%2!=0){
-            console.log(bir + ' toq son');
+        if(index%2!=0){
+            console.log(index + ' toq son');
         }
     }
 } 
-showNumbers(15)*/
+let array = [1, 6, 7]
+showNumbers(array)*/
 
 // 5 - misol
 /*function Truthy(nimadir){
@@ -171,27 +172,570 @@ for(let item in obj2) {
     }
 }*/
 
+// Landscape Portrait
+/*function Determinatior(boyi, eni){
+    if (boyi > eni){
+        console.log('Portrait mode');
+    }
+    else {
+        console.log('Landscape mode');
+    }
+}
+Determinatior(12, 25)*/
 
+// if 1 - misol
+/*let son = -5
+if (son > 0) {
+    son ++
+}
+console.log(son);*/
 
+// if 2 - misol
+/*let son = -2
+if (son > 0) {
+    son ++
+}
+else {
+    son -= 2
+}
+console.log(son);*/
 
+// if 3 - misol
+/*let son = 0
+if (son > 0) {
+    son ++
+}
+if (son < 0) {
+    son -= 2
+}
+else {
+    son = 10
+}
+console.log(son);*/
 
+// if 4 - misol
+/*const son1 = 5
+const son2 = -5
+const son3 = 1
+let sanoq = 0
+if (son1 > 0) {
+    sanoq += 1
+}
+if (son2 > 0) {
+    sanoq += 1
+}
+if (son3 > 0) {
+    sanoq += 1
+}
+console.log('musbat sonlar jami ' + sanoq + 'ta');*/
 
+// if 5 - misol
+/*const son1 = 5
+const son2 = -5
+const son3 = 0
+let musbat = 0
+let manfiy = 0
+if (son1 >= 0) {
+    musbat += 1
+}
+if (son2 >= 0) {
+    musbat += 1
+}
+if (son3 >= 0) {
+    musbat += 1
+}
+if (son1 < 0) {
+    manfiy += 1
+}
+if (son2 < 0) {
+    manfiy += 1
+}
+if (son3 < 0) {
+    manfiy += 1
+}
+console.log('musbat sonlar jami ' + musbat + 'ta\n' + 'manfiy son jami ' + manfiy + 'ta');*/
 
+// if 6 - misol
+/*const son1 = 5
+const son2 = 6
+if (son1 > son2) {
+    console.log('kottasi ' + son1);
+}
+else {
+    console.log('kottasi ' + son2);
+}*/
 
+// 7 - misol
+/*const son1 = 5
+const son2 = 4
+if (son1 < son2) {
+    console.log(son1 + ' kichkina');
+}
+else {
+    console.log(son2 + ' kichkina');
+}*/
 
+// 8 - misol
+/*const son1 = 5
+const son2 = 10
+if (son1 > son2) {
+    console.log(son1 + ' kottasi\n' + son2 + ' kichkinasi');
+}
+else {
+    console.log(son2 + ' kottasi\n' + son1 + ' kichkinasi');
+}*/
 
+// 9 - misol
+/*let son1 = 3
+let son2 = 4
+son1 += son2
+son2 = son1 - son2
+son1 = son1 - son2
+console.log(son1 + ' <-- son1', son2 + ' <-- son2');*/
 
+// 10 - misol
+/*let son1 = 1
+let son2 = 4
+if (son1 != son2) {
+    son1 += son2
+    son2 = son1
+}
+else {
+    son1 = 0
+    son2 = 0
+}
+console.log(son1, son2);*/
 
+// 11 - misol
+/*let son1 = 4
+let son2 = 4
+if (son1 != son2) {
+    if (son1 > son2) {
+        son2 = son1
+    }
+    else {
+        son1 = son2
+    }
+}
+else {
+    son1 = 0
+    son2 = 0
+}
+console.log(son1, son2);*/
+// 12 - misol
+/*const son1 = 4
+const son2 = -6
+const son3 = -5
+if (son1 < son2 && son1 < son3) {
+    console.log(son1);
+}
+if (son2 < son1 && son2 < son3) {
+    console.log(son2);
+}
+else {
+    console.log(son3);
+}*/
 
+// 13 - misol
+/*const son1 = -5.5
+const son2 = -6
+const son3 = -5
+if (son1 > son2 && son1 < son3 || son1 > son3 && son1 < son2) {
+    console.log(son1);
+}
+else if (son2 > son1 && son2 < son3 || son2 > son3 && son2 < son1) {
+    console.log(son1);
+}
+else {
+    console.log(son3);
+}*/
 
+// 14 - misol
+/*const son1 = 5
+const son2 = -6
+const son3 = -5
+if (son1 < son2 && son1 < son3) {
+    console.log(son1);
+    if (son2 < son3) {
+        console.log(son2);
+        console.log(son3);
+    }
+    else {
+        console.log(son3);
+        console.log(son2);
+    }
+}
+if (son2 < son1 && son2 < son3) {
+    console.log(son2);
+    if (son1 < son3) {
+        console.log(son1);
+        console.log(son3);
+    }
+    else {
+        console.log(son3);
+        console.log(son1);
+    }
+}
+else {
+    console.log(son3);
+    if (son1 < son2) {
+        console.log(son1);
+        console.log(son2);
+    }
+    else {
+        console.log(son2);
+        console.log(son1);
+    }
+}*/
 
+// 15 - misol
+/*const son1 = 5
+const son2 = 4
+const son3 = -5
+if (son1 > son2 && son1 > son3) {
+    console.log(son1);
+    if (son2 > son3) {
+        console.log(son2);
+    }
+    else {
+        console.log(son3);
+    }
+}
+else if (son2 > son1 && son2 > son3) {
+    console.log(son2);
+    if (son1 > son3) {
+        console.log(son1);
+    }
+    else {
+        console.log(son3);
+    }
+}
+else {
+    console.log(son3);
+    if (son1 > son2) {
+        console.log(son1);
+    }
+    else {
+        console.log(son2);
+    }
+}*/
 
+// 16 - misol
+/*let son1 = 1
+let son2 = 6
+let son3 = 3
+if (son1 < son2 && son1 < son3 && son2 > son1 && son2 < son3 && son3 > son1 && son3 > son1) {
+    son1 *= 2
+    son2 *= 2
+    son3 *= 2
+}
+else {
+    son1 -= (son1 * 2)
+    son2 -= (son2 * 2)
+    son3 -= (son3 * 2)
+}
+console.log(son1, son2, son3);*/
 
+// 17 - misol
+/*let son1 = 1
+let son2 = 2
+let son3 = 3
+if (son1 > son2 && son1 > son3 && son2 < son1 && son2 > son3 && son3 < son1 && son3 < son1) {
+    son1 *= 2
+    son2 *= 2
+    son3 *= 2
+}
+else if (son1 < son2 && son1 < son3 && son2 > son1 && son2 < son3 && son3 > son1 && son3 > son1) {
+    son1 *= 2
+    son2 *= 2
+    son3 *= 2
+}
+else {
+    son1 -= (son1 * 2)
+    son2 -= (son2 * 2)
+    son3 -= (son3 * 2)
+}
+console.log(son1, son2, son3);*/
 
+// 18 - misol
+/*const son1 = 4
+const son2 = 4
+const son3 = 1
+if (son1 != son2 && son1 != son3) {
+    console.log(son1);
+}
+if (son2 != son1 && son2 != son3) {
+    console.log(son2);
+}
+else {
+    console.log(son3);
+}*/
 
+// 19 - misol
+/*const son1 = 4
+const son2 = 4
+const son3 = 1
+const son4 = 4
+if (son1 != son2 && son1 != son3 && son1 != son4) {
+    console.log(son1);
+}
+if (son2 != son1 && son2 != son3 && son2 != son4) {
+    console.log(son2);
+}
+if (son3 != son1 && son3 != son2 && son3 != son4) {
+    console.log(son3);
+}
+else {
+    console.log(son4);
+}*/
 
+// 20 - misol
+/*let A = 4
+let B = 8
+let C = 6
+let bniki = B - A
+let cniki = C - A
+if (bniki < cniki) {
+    console.log(B + ' A ga yaqini shu');
+}
+else {
+    console.log(C + ' A ga yaqini shu');
+}*/
 
+// 21 - misol
+/*let array = [2, 0]
+if (array[0] == 0 && array[1] == 0) {
+    console.log(0);
+}
+else if (array[0] != 0 && array[1] == 0) {
+    console.log(1);
+}
+else if (array[0] == 0 && array[1] != 0) {
+    console.log(2);
+}
+else {
+    console.log(3);
+}*/
 
+// 22 - misol
+/*const array = [9, -4]
+if (array[0] > 0 && array[1] > 0) {
+    console.log('1 - chorakda yotipti');
+}
+else if (array[0] < 0 && array[1] > 0) {
+    console.log('2 - chorakda yotipti');
+}
+else if (array[0] < 0 && array[1] < 0) {
+    console.log('3 - chorakda yotipti');
+}
+else {
+    console.log('4 - chorakda yotipti');
+}*/
+
+// 23 - misol
+/*const uchi1 = -4
+const uchi2 = 0
+const uchi3 = 4
+let array = []
+array.push(uchi1)
+array.push(uchi3)
+console.log('4 chi uchi ' + array);*/
+
+// 24 - misol
+/*function hisoblash(x) {
+    if (x > 0) {
+        x *= 2
+    }
+    else {
+        x -= 6
+    }
+    console.log(x);
+}
+hisoblash(-6)*/
+
+// 25 - misol
+/*function hisoblash(x) {
+    if (x < -2 || x > 2) {
+        x *= 2
+    }
+    else {
+        x *= -3
+    }
+    console.log(x);
+}
+hisoblash(1)*/
+
+// 26 - misol
+/*function hisoblash(x) {
+    if (x <= 0) {
+        x -= (x * 2)
+    }
+    else if (x > 0 && x < 2) {
+        x *= x
+    }
+    if (x >= 2) {
+        x = 4
+    }
+    console.log(x);
+}
+hisoblash(0)*/
+
+// 27 - misol
+/*function hisoblash(x) {
+    if (x < 0) {
+        x = 0
+    }
+    else if (x % 2 == 0 && x == 0) {
+        x = 1
+    }
+    else if (x % 2 != 0) {
+        x = -1
+    }
+    console.log(x);
+}
+hisoblash(3)*/
+
+// 28 - misol
+/*let yil = 2020
+if (yil % 400 == 0) {
+    console.log('kabisa yil');
+}
+else if (yil % 4 == 0 && yil % 100 != 0) {
+    console.log('kabisa yil');
+}
+else {
+    console.log('kabisa yil emas');
+}*/
+
+// 29 - misol
+/*let son = 0
+if (son == 0) {
+    console.log('son nolga teng');
+}
+else if (son = 0) {
+    if (son % 2 ==0) {
+        console.log('musbat juft son');
+    }
+    else {
+        console.log('musbat toq son');
+    }
+}
+else {
+    if (son % 2 ==0) {
+        console.log('manfiy juft son');
+    }
+    else {
+        console.log('manfiy toq son');
+    }
+}*/
+
+// 30 - misol
+/*let son = 112
+if (son < 100) {
+    if (son % 2 == 0) {
+        console.log('2 xonali juft son');
+    }
+    else {
+        console.log('2 xonali toq son');
+    }
+}
+else {
+    if (son % 2 == 0) {
+        console.log('3 xonali juft son');
+    }
+    else {
+        console.log('3 xonali toq son');
+    }
+}*/
+
+// 1 - misol
+/*let arr_1 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; 
+let arr_2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26];
+let yigindisi = 0
+for (let item of arr_1) {
+    yigindisi += item
+}
+for (let item of arr_2) {
+    yigindisi += item
+}
+console.log(yigindisi);*/
+
+// 2 - misol
+/*const son = 22
+let soz = ''
+for (let juft = 2; juft <= son; juft += 2) {
+    soz += juft + ' '
+}
+console.log(soz);*/
+
+// 3 - misol
+/*let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+let soz = ''
+for (let item of arr.reverse()){
+    soz += item + ' '
+}
+console.log(soz);*/
+
+// 4 - misol
+/*let arr_3 = [4, 6, 7];
+let arr_4 = [8, 1, 9];
+let arr_1 = []
+for (let item = 0; item < arr_4.length; item++){
+    arr_1.push(arr_3[item ] + arr_4[item])
+}
+console.log(arr_1);*/
+
+// 5 - misol
+/*let str1 = "javascript";
+let str2 = ''
+for (let item = 0; item < str1.length; item++) {
+    if (item % 2 != 0) {
+        str2 += 'Z'
+    }
+    else {
+        str2 += str1[item]
+    }
+}
+console.log(str2);*/
+
+// 6 - misol
+/*let str2 = "don’t know why";
+let y = 'y'
+if (str2.includes(y)) {
+    console.log('yes');
+}
+else {
+    console.log('no');
+}*/
+
+// 7 - misol
+/*let n2 = 4;
+let factorial = 1
+for (let bir = 1; bir <= n2; bir++) {
+    factorial *= bir
+}
+console.log(factorial);*/
+
+// 9 - misol
+/*let str3 = "racecar";
+let str1 = ''
+for (let item = str3.length - 1; item >= 0; item--) {
+    str1 += str3[item]
+}
+if (str3 == str1) {
+    console.log('Palindrome');
+}
+else {
+    console.log('Not Palindrome');
+}*/
+
+// 10 - misol
+/*let son = 8
+let yigindisi = 0
+for (let bir = 1; bir <= son; bir++) {
+    yigindisi += bir
+}
+console.log(yigindisi);*/
 
 
 
